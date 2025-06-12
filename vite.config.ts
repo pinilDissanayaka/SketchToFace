0505572df62ff7e3c44c4fb2631b0f64c679629b
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-06-12 07:18:35 +0530

    add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: '/SketchToFace/',  // <-- add this line
   plugins: [
     tailwindcss(),
   ],
